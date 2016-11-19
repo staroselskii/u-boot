0202097bacc59e10797453cbe4831f44439b6d8e
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2016-11-19 20:25:13 +0200

    Edison x86 : Enable SD card.
    Signed-off-by: eric.park <eric.park@intel.com>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- arch/x86/cpu/tangier/tangier.c
+++ arch/x86/cpu/tangier/tangier.c
@@ -8,11 +8,13 @@
 
 #include <common.h>
 #include <asm/arch/intel-mid.h>
+#include <asm/arch/intel_soc_mrfld.h>
 #include <asm/arch/mmc.h>
 #include <asm/u-boot-x86.h>
 #include <asm/cache.h>
 #include <asm/io.h>
 #include <asm/msr.h>
+#include <linux/errno.h>
 #include <intel_scu_ipc.h>
 #include <u-boot/md5.h>
 
@@ -31,6 +33,66 @@ int board_early_init_f(void)
 	return 0;
 }
 
+static int pmu_read_status(void)
+{
+	struct mrst_pmu_reg *const pmu_reg =
+		(struct mrst_pmu_reg *)CONFIG_SYS_TANGIER_PMU_BASE;
+	int pmu_busy_retry = 500000;
+	u32 temp;
+	union pmu_pm_status result;
+
+	do {
+		temp = readl(&pmu_reg->pm_sts);
+
+		/* extract the busy bit */
+		result.pmu_status_value = temp;
+
+		if (result.pmu_status_parts.pmu_busy == 0)
+			return 0;
+
+		udelay(1);
+	} while (--pmu_busy_retry);
+
+	return -EBUSY;
+}
+
+int power_init_board(void)
+{
+	struct mrst_pmu_reg *const pmu_reg =
+		(struct mrst_pmu_reg *)CONFIG_SYS_TANGIER_PMU_BASE;
+	u32 pm_ssc[4];
+	int i;
+
+	/* check pmu status. */
+	if (pmu_read_status()) {
+		printf("WARNING : PMU is busy.\n");
+		return -EBUSY;
+	}
+
+	/* read pmu values. */
+	for (i = 0 ; i < 4 ; i++)
+		pm_ssc[i] = readl(&pmu_reg->pm_sss[i]);
+
+	/* modify pmu values. */
+	/* enable SDIO0, sdhci for SD card*/
+	pm_ssc[0] &= ~(0x3<<(PMU_SDIO0_LSS_01*2));
+
+	/* wrtie modified pmu values. */
+	for (i = 0 ; i < 4 ; i++)
+		writel(pm_ssc[i], &pmu_reg->pm_ssc[i]);
+
+	/* update modified pmu values. */
+	writel(0x00002201, &pmu_reg->pm_cmd);
+
+	/* check pmu status. */
+	if (pmu_read_status()) {
+		printf("WARNING : PMU is busy.\n");
+		return -EBUSY;
+	}
+
+	return 0;
+}
+
 int board_early_init_r(void)
 {
 	init_scu_ipc();
@@ -39,14 +101,12 @@ int board_early_init_r(void)
 
 int board_final_cleanup(void)
 {
-
 	return 0;
 }
 
 int board_late_init(void)
 {
 	if (!getenv("serial#")) {
-
 		struct mmc *mmc = find_mmc_device(0);
 		unsigned char emmc_ssn[16];
 		char ssn[33];
@@ -73,11 +133,10 @@ int board_late_init(void)
 
 	if (!getenv("hardware_id")) {
 		union ipc_ifwi_version v;
-		int ret;
 		char hardware_id[4];
+		int ret;
 
-		ret = intel_scu_ipc_command(IPCMSG_GET_FW_REVISION, 1,
-				NULL, 0, (u32 *) &(v.raw[0]), 4);
+		ret = intel_scu_ipc_command(IPCMSG_GET_FW_REVISION, 1, NULL, 0, (u32 *)v.raw, 4);
 		if (ret < 0) {
 			printf("Can't retrieve hardware revision\n");
 		}
@@ -88,7 +147,6 @@ int board_late_init(void)
 		saveenv();
 #endif
 	}
-
 
 	return 0;
 }
@@ -104,10 +162,15 @@ int print_cpuinfo(void)
 
 int board_mmc_init(bd_t * bis)
 {
-	int index = 0;
-	unsigned int base = CONFIG_SYS_EMMC_PORT_BASE + (0x40000 * index);
+	int err;
 
-	return tangier_sdhci_init(base, index, 4);
+	/* add sdhci for eMMC */
+	err = tangier_sdhci_init(CONFIG_SYS_EMMC_PORT_BASE, 0, 4);
+	if (err)
+		return err;
+
+	/* add sdhci for SD card */
+	return tangier_sdhci_init(CONFIG_SYS_SD_PORT_BASE, 0, 4);
 }
 
 void reset_cpu(ulong addr)
